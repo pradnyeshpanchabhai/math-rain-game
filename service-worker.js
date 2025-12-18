0bdc621f6cca156bc91ea6757ffd8e2be37a7981
pradnyesh panchabhai <165567577+pradnyeshpanchabhai@users.noreply.github.com>
2025-12-18 13:53:55 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
+document.getElementById("startBtn").onclick = () => {
+  document.getElementById("correctSound").play();
+  document.getElementById("wrongSound").play();
+  document.getElementById("tapSound").play();
+
+  document.getElementById("startBtn").style.display="none";
+};
+
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open("math-rain-cache").then(function(cache) {
